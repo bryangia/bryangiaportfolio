741f181eb7b4e1efa8a329e61ef065370c848d5c
bryangia <bryanthenguyener@gmail.com>
2021-06-04 12:20:19 -0500

    changing ref link

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://cashapp-transaction-tracker.herokuapp.com/src/index.php"
           target="_blank"
           rel="noopener noreferrer"
         >
